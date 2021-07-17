c4e341a4e16da1e04ac35732c63773401d94bcd4
hyeongju <kokiguitars@gmail.com>
2021-07-17 23:14:00 +0900

    Add SmallCard, SmallCardDisplay

---
+++ components/SmallCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img :src="require(`@/assets/images/${card.image || 'fe1.jpeg'}`)" alt class="image" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["card"]
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,15 +6,18 @@
       :key="cardInfo.id"
       :cardsSection="cardInfo"
     />
+    <SmallCardDisplay v-for="cardInfo in smallCardInfo" :key="cardInfo.id" :cardSection="cardInfo" />
   </div>
 </template>
 
 <script>
 import { largeCardSections } from "@/assets/data.js";
+import { smallCardSections } from "@/assets/data.js";
 export default {
   data() {
     return {
-      largeCardInfo: largeCardSections
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
     };
   }
 };
